56108df58898bf13b86ac79a3a876813908a3c1a
Ayush Vardhan <ayushvardhan1795@gmail.com>
2018-12-15 22:17:03 +0530

    JSX and Hello World in React

---
+++ React/151218/helloworld/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+const name = 'Ayush Vardhan';
+const element = <h1>Hello, {name}</h1>;
+
+// Below is the original built-in code
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(element, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
